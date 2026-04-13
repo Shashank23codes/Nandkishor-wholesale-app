5b14e9c8a822b5ab2e1bdc2bd518ae2e3a4dd1bd
Shashank23codes <shashankgupta4068@gmail.com>
2026-04-13 14:15:43 +0530

    fix: set base path to relative to resolve production loading issues in Electron

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // CRITICAL fix for Electron production black screen
   plugins: [react(), tailwindcss()],
 })
